0431ee0f9e82307d008601b890593af3e1176471
MarcoCavani <cavaniaustralia@gmail.com>
2021-09-09 19:02:19 +0800

    master

--- js/blob.js
+++ js/blob.js
@@ -47,7 +47,7 @@ class Blob {
       ctx.quadraticCurveTo(p1.x, p1.y, xc, yc);
       // ctx.lineTo(p2.x, p2.y);
 
-      ctx.fillStyle = '#01204d ';
+      ctx.fillStyle = '#ffffff';
       // ctx.fillRect(p1.x-2.5, p1.y-2.5, 5, 5);
 
       p1 = p2;
@@ -61,11 +61,11 @@ class Blob {
     // ctx.closePath();
     ctx.fillStyle = this.color;
     ctx.fill();
-    ctx.strokeStyle = '#01204d ';
+    ctx.strokeStyle = '#ffffff';
     // ctx.stroke();
     
 /*
-    ctx.fillStyle = '#01204d ';
+    ctx.fillStyle = '#ffffff';
     if(this.mousePos) {
       let angle = Math.atan2(this.mousePos.y, this.mousePos.x) + Math.PI;
       ctx.fillRect(center.x + Math.cos(angle) * this.radius, center.y + Math.sin(angle) * this.radius, 5, 5);
@@ -84,7 +84,7 @@ class Blob {
     this._color = value;
   }
   get color() {
-    return this._color || '#01204d ';
+    return this._color || '#ffffff';
   }
   
   set canvas(value) {
